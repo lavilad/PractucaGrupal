faa1c861147a6181e479ee078e7dc8d3c02da821
Santiago s <santiago.soria@edu.uag>
2026-03-05 13:29:45 -0600

    Santiago Soria agrego  mi linea

--- vacio.py
+++ vacio.py
@@ -1,2 +1,3 @@
 "Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
 print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
+"Nombre: Santiago Soria Alvarez | Carrera: Ing. Automotriz | Jugar futbol"
