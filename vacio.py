8db8d5b03d1dbd4199cc804fc37fe79e3fa019b8
Rosendo López <rlop2086@gmail.com>
2026-03-05 13:31:11 -0600

    Rosendo: agrego mi linea

--- vacio.py
+++ vacio.py
@@ -1,2 +1,3 @@
-"Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
-print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
+#"Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
+#print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
+# ("Nombre: Rosendo López Martines   | Carrea: Ing. Automotriz   Hobby: Jugar videojuegos y escuchar musica")
