a2ef996f525dff2c08494e3a0368a704bdc2c466
luis <luis.araiza@edu.uag.mx>
2026-03-05 13:34:54 -0600

    Luis Angel Araiza Suarez agregue mi linea

--- vacio.py
+++ vacio.py
@@ -1,2 +1,3 @@
 "Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
 print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
+"nombre: luis angel araiza suarez| Carrera: Ing. Automotriz | Hobby: Jugar trabajar en autos"
