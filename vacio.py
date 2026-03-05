897a79689bddb8987694de6af49229ac1ffa060c
Santiago Valle <santiagovalleg2006@gmail.com>
2026-03-05 13:34:59 -0600

    Santiago agrego mi linea

--- vacio.py
+++ vacio.py
@@ -4,3 +4,4 @@ print("Nombre: ian cota | Carrera: Ing. Automotriz | Hobby:: jugar futbol")
 "Nombre: Luis Josue Lopez De la Toba | Carrera: Ing. Automotriz | Hobby: Deportes")
 print("Nombre: Carlos Macías Ruiz | Carrera: Ing. Automotriz | Hobby: Jugar videojuegos")
 >>>>>>> 8d4eebc68ac3e2e8c629314f4643d039c12ac7e8
+"Nombre: Santiago Valle Garnica | Carrera: Ing. Automotriz | Hobby:Hacer deporte"
